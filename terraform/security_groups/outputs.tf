9e9e2b8a5c8ee78ab261c66c26d8d9bdd653aed4
Abir Datta <abirdatta.in@gmail.com>
2019-10-28 13:35:22 +0530

    added outputs in security group

--- terraform/security_groups/outputs.tf
+++ terraform/security_groups/outputs.tf
@@ -1,3 +1,15 @@
 output "rds_db_security_group_id" {
   value = module.rds_db_security_group.security_group_id
 }
+
+output "rds_alb_security_group_id" {
+  value = module.alb_security_group.security_group_id
+}
+
+output "rds_ecredis_security_group_id" {
+  value = module.elasticache_redis_security_group.security_group_id
+}
+
+output "rds_ec2_security_group_id" {
+  value = module.ec2_security_group.security_group_id
+}
